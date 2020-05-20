14d04cc527701e017de0777658557b7a3f9c4f66
rrojasPy <rrojas@paraguayedica.org>
2020-05-20 16:54:59 -0400

    up

---
+++ extra.sh
@@ -0,0 +1,31 @@
+#!/bin/sh root
+
+
+sudo add-apt-repository ppa:jclic/master
+
+
+
+sudo apt update
+sudo apt-get update
+apt upgrade
+echo "1/15"
+yes | apt install python-pip
+echo "2/15"
+yes | apt install wget
+echo "3/15"
+yes | apt install unzip
+yes | apt install python-requests
+yes | apt install vim
+yes | apt install geogebra
+yes | apt install gcompris-qt
+yes | apt install tuxpaint
+echo "10/15"
+yes | apt install etoys
+yes | apt install gir1.2-telepathyglib-0.12
+yes | apt install jclic
+yes | apt install default-jre
+yes | apt install default-jdk
+yes | apt install x11vnc
+yes | apt install gufw
+sudo apt-get update
+echo "FULL"
