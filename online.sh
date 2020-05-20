1fc1532bd52054f42053e3f389f7a605dbf2957e
rrojasPy <rrojas@paraguayedica.org>
2020-05-20 16:48:29 -0400

    Herramientas

---
+++ online.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+apt update
+apt upgrade
+apt install -y python-pip
+apt install -y wget
+apt install -y unzip
+apt install -y python-requests
+apt install -y vim
+apt install -y geogebra
+apt install -y gcompris-qt
+apt install -y tuxpaint
+apt install -y etoys
+apt install -y gir1.2-telepathyglib-0.12
+sudo add-apt-repository ppa:jclic/master
+sudo apt-get update
+apt install -y jclic
+apt install -y default-jre
+apt install -y default-jdk
+apt install -y x11vnc
+apt install -y gufw
