49ae83957b1283039eeba14cff4008d578ea619b
RodPy <rodneyrrs@hotmail.com>
2020-06-16 23:04:38 -0400

    Add Node Instal

---
+++ NodeRedInstall.sh
@@ -0,0 +1,7 @@
+##! /bin/bash sh
+
+echo "Install Node Red Raspberry"
+
+
+bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered) -y
+ 
